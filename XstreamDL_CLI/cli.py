2df1be08013f5af8a55a0ec16cc0fde52f19f098
thanioruvangkls <91356119+thanioruvangkls@users.noreply.github.com>
2022-07-19 13:29:45 +0530

    Update cli.py

--- XstreamDL_CLI/cli.py
+++ XstreamDL_CLI/cli.py
@@ -71,9 +71,9 @@ def command_handler(args: CmdArgs):
             args.mp4decrypt = (bin_path / 'mp4decrypt.exe').resolve().as_posix()
             args.mp4box = (bin_path / 'mp4box.exe').resolve().as_posix()
         else:
-            args.ffmpeg = (bin_path / 'ffmpeg').resolve().as_posix()
-            args.mp4decrypt = (bin_path / 'mp4decrypt').resolve().as_posix()
-            args.mp4box = (bin_path / 'mp4box').resolve().as_posix()
+            args.ffmpeg = ('%s/ffmpeg').resolve().as_posix()
+            args.mp4decrypt = ('%s/mp4decrypt').resolve().as_posix()
+            args.mp4box = ('%s/mp4box').resolve().as_posix()
     logger.debug(f'ffmpeg {args.ffmpeg}')
     logger.debug(f'mp4decrypt {args.mp4decrypt}')
     logger.debug(f'mp4box {args.mp4box}')
